0dab4d00d8a69e57ae7d1f8e06caf332a8ab754a
19-SDEV-CMDI-RPOL <FDSAP-RRPOLINTANG@19-SDEV-CMDI-RPOL.local>
2024-09-11 11:07:30 +0800

    rename

--- database scripts/tables/logs-schema-tables.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE logs.user_tokens (
-        token_id serial primary key,
-        username TEXT,
-        staff_id TEXT, 
-        token TEXT,
-        insti_code TEXT,
-        app_code TEXT,
-        created_at timestamp default current_timestamp
-);
