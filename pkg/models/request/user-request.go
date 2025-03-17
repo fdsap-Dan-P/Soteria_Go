6bb2c100b58aef55f1e419aa736ebcbcd2d45125
19-SDEV-CMDI-RPOL <FDSAP-RRPOLINTANG@19-SDEV-CMDI-RPOL.local>
2025-03-17 08:46:00 +0800

    CHANGED: add omitempty in stkruct

--- pkg/models/request/user-request.go
+++ pkg/models/request/user-request.go
@@ -3,14 +3,14 @@ package request
 type (
 	UserRegistrationRequest struct {
 		Username         string `json:"username"`
-		First_name       string `json:"first_name"`
-		Middle_name      string `json:"middle_name"`
-		Last_name        string `json:"last_name"`
-		Email            string `json:"email"`
-		Phone_no         string `json:"phone_no"`
-		Staff_id         string `json:"staff_id"`
-		Institution_code string `json:"institution_code"`
-		Birthdate        string `json:"birthdate"`
+		First_name       string `json:"first_name,omitempty"`
+		Middle_name      string `json:"middle_name,omitempty"`
+		Last_name        string `json:"last_name,omitempty"`
+		Email            string `json:"email,omitempty"`
+		Phone_no         string `json:"phone_no,omitempty"`
+		Staff_id         string `json:"staff_id,omitempty"`
+		Institution_code string `json:"institution_code,omitempty"`
+		Birthdate        string `json:"birthdate,omitempty"`
 	}
 
 	LoginCredentialsRequest struct {
