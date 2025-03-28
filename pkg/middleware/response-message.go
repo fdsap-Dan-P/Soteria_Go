83fc606958ed312cbb847b6293e9f08d34dd575d
19-SDEV-CMDI-RPOL <FDSAP-RRPOLINTANG@19-SDEV-CMDI-RPOL.local>
2025-03-28 10:23:18 +0800

    CHANGED: update console log format

--- pkg/middleware/response-message.go
+++ pkg/middleware/response-message.go
@@ -52,8 +52,27 @@ func ResponseData(username, instiCode, appCode, moduleName, funcName, retcode, m
 		returnMessage.Data.Details = details
 	}
 
-	fmt.Println("")
-	fmt.Printf("CAGABAY UA | %s | %s | Error: %+v | %s | %s |", returnMessage.RetCode, returnMessage.Data.Message, returnMessage.Data.Error, funcName, endpoint)
+	// fmt.Println()
+	// fmt.Println("============== FAILED REQUEST =============")
+	// fmt.Println("REF ID: ", refID)
+	fmt.Println("============= REQUEST DETAILS =============")
+	fmt.Println("USERNAME: ", username)
+	fmt.Println("FUNCTION NAME: ", funcName)
+	// if funcName != subFuncName {
+	// 	fmt.Println("SUB FUNCTION NAME: ", subFuncName)
+	// }
+	fmt.Println("METHOD: ", method)
+	fmt.Println("ENDPOINT: ", endpoint)
+	fmt.Println("REQUEST BDDY: ", string(reqBody))
+
+	// Response Information
+	fmt.Println("============ RESPONSE DETAILS =============")
+	fmt.Println("RET CODE: ", returnMessage.RetCode)
+	fmt.Println("RESPONSE CATEGORY: ", returnMessage.Message)
+	fmt.Println("RESPONSE MESSAGE: ", returnMessage.Data.Message)
+	fmt.Println("RESPONSE MESSAGE: ", string(respBsody))
+	fmt.Println("ERROR MESSAGE: ", returnMessage.Data.Error)
+	fmt.Println()
 
 	ActivityLogger(username, instiCode, appCode, moduleName, funcName, retcode, method, endpoint, []byte(reqBody), []byte(""), returnMessage.Message, returnMessage.Data.Message, error_message)
 	return returnMessage
